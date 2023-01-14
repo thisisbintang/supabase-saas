16d29fc6a42ef2f15e6c2c0b62b949abeb686d0a
Bima Maulanam <amibanaluam1@gmail.com>
2023-01-14 19:32:10 +0700

    dumy

--- context/auth.tsx
+++ context/auth.tsx
@@ -1,52 +1,40 @@
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
 import React, { PropsWithChildren, useCallback, useContext, useEffect, useState } from "react";
+import { LoginBody } from "../pages/login";
+import { RegisterBody } from "../pages/register";
 import { Database } from "../types/supabase";
 
 const Context = React.createContext<{
     login: () => void;
     logout: () => void;
-    profile: Profile | null;
+    loginWithEmail: (data: LoginBody) => void;
+    register: (data: RegisterBody) => void;
+    student: Students | null;
 }>({
     login: () => { },
     logout: () => { },
-    profile: null
+    register: () => { },
+    loginWithEmail: () => { },
+    student: null
 });
 
-type Profile = Database['public']['Tables']['profiles']['Row']
+type Students = Database['public']['Tables']['students']['Row']
 
 export default function AuthProvider({ children }: PropsWithChildren) {
     const supabaseClient = useSupabaseClient<Database>()
     const user = useUser()
     const router = useRouter()
-    const [profile, setProfile] = useState<Profile | null>(null)
+    const [student, setStudent] = useState<Students | null>(null)
 
     useEffect(() => {
         if (user) {
-            const channel = supabaseClient.channel(`public:profiles:id=eq.${user.id}`)
-                .on('postgres_changes', {
-                    event: 'UPDATE',
-                    schema: 'public',
-                    table: 'profiles',
-                    filter: `id=eq.${user.id}`
-                }, (payload) => {
-                    setProfile(payload.new as any)
-                }).subscribe()
-
-            return () => {
-                supabaseClient.removeChannel(channel)
-            }
-        }
-    }, [user]);
-
-    useEffect(() => {
-        if (user) {
-            const fetchProfile = async () => {
-                const { data } = await supabaseClient.from('profiles').select('*').eq('id', user.id).single()
-                setProfile(data);
+            const fetchStudent = async () => {
+                const { data } = await supabaseClient.from('students').select('*').eq('id', user.id).single()
+                setStudent(data);
             }
 
-            fetchProfile()
+            fetchStudent()
         }
     }, [user])
 
@@ -56,16 +44,57 @@ export default function AuthProvider({ children }: PropsWithChildren) {
         })
     }, [])
 
+    const loginWithEmail = useCallback(async (data: LoginBody) => {
+        const { data: { user }, error } = await supabaseClient.auth.signInWithPassword({
+            email: data.email,
+            password: data.password,
+        })
+
+        if (error) return alert(error.message)
+
+        // ceklokasi apakah di sekitran kampus
+
+        // kala iya
+        await supabaseClient.from('students').update({ status_login: true }).eq('id', user?.id);
+        router.push('/dashboard')
+
+    }, [])
+
     const logout = useCallback(async () => {
+        const { data: { user } } = await supabaseClient.auth.getUser();
+
+        await supabaseClient.from('students').update({ status_login: false }).match({ id: user?.id });
+
         await supabaseClient.auth.signOut();
         router.push('/login')
+    }, [])
+
+    const register = useCallback(async (data: RegisterBody) => {
+
+        const { data: result, error } = await supabaseClient.auth.signUp({
+            email: data.email,
+            password: data.password,
+            options: {
+                data: {
+                    angkatan: data.angkatan,
+                    prodi: data.prodi,
+                    nim: data.nim,
+                    no_hp: data.no_hp,
+                    nama: data.nama,
+                }
+            }
+        })
+
+        if (!error) router.push('confirm-email')
     }, [])
 
     return (
         <Context.Provider value={{
             login,
             logout,
-            profile,
+            student,
+            register,
+            loginWithEmail,
         }}>
             {children}
         </Context.Provider>
---
+++ pages/confirm-email.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConfirtmationEmail = () => {
+    return (
+        <div>Please check your email and confirmation your account</div>
+    )
+}
+
+export default ConfirtmationEmail
